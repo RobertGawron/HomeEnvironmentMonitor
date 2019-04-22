1aef490ffd58e1b73b929fd72b5eb9e3c5e56d06
= <=>
2019-04-22 12:30:10 +0100

    removed hardcoded rendering

--- HomeEnviromentMonitorServer/datalogger/views.py
+++ HomeEnviromentMonitorServer/datalogger/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import TemperatureMeassurement
+
 # Create your views here.
 
 def index(request):
     temperature_list = TemperatureMeassurement.objects.order_by('-sample_date')
-    output = ', '.join([str(q.sample_value) for q in temperature_list])
-    return HttpResponse(output)
+    #output = ', '.join([str(q.sample_value) for q in temperature_list])
+    template = loader.get_template('datalogger/index.html')
+    context = {
+        'temperature_list' : temperature_list,
+    }
+    return HttpResponse(template.render(context, request))
