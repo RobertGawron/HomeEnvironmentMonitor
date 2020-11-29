2f6823a5ce423e2f193a274972f377642b6e3fce
Robert Gawron <robert_gawron@yahoo.fr>
2020-11-29 17:14:21 +0100

    Draft of ploting graphs using https://nvd3.org/

--- Server/Server/GeigerCounter/views.py
+++ Server/Server/GeigerCounter/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 from .models import Measurement
 
-
+import datetime
 
 def index(request):
-    measurements = Measurement.objects.order_by('-date')
+    measurements = Measurement.objects.order_by('date')
+
+    ## TODO this is dummy way, it's just to see if things works
+    i=0
+    for m in measurements:
+        m.date = i
+        i+=1
 
     context = {'measurements': measurements}
     return render(request, 'GeigerCounter/index.html', context)
